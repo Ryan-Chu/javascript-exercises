de9eaef243acff97e50411509e5c64952e07653e
Ryan Chu <Ryan.chu1688@gmail.com>
2020-07-25 02:08:41 -0700

    answers to calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add (a,b) {
+	return a+b;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((total, current) => total + current, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
 }
 
-function power() {
-	
+function power(a,b) {
+	return Math.pow(a,b)
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n == 0) return 1;
+	let answer = 1;
+	for (let i = n; i > 0; i--){
+		answer *= i;
+	}
+	return answer;
 }
 
 module.exports = {
