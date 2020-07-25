186845300b9ee55c5e0591e8c0c460a9b8e5e723
Ryan Chu <Ryan.chu1688@gmail.com>
2020-07-25 02:25:38 -0700

    answers for fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(numberOffibonacci) {
+    if(numberOffibonacci < 0) return "OOPS";
+    if (numberOffibonacci == 0) return 0;
+    var x = 1;
+    var y = 1;
+    for (var i = 0; i < numberOffibonacci - 2; i ++){
+        if( i % 2 == 0){
+            x = x + y;
+        }
+        else{
+            y = x + y;
+        }
+    }
+    if(x>y){
+        return x;
+    }
+    else{
+        return y;
+    }
 }
 
 module.exports = fibonacci
