72782d5d8962ae2a35448e2e6369857b27c637de
Ryan Chu <Ryan.chu1688@gmail.com>
2020-07-29 01:41:54 -0700

    answers to palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(wordChecker) {
+    processed = wordChecker.toLowerCase().replace(/[^A-Za-z]/g,"");
+    return (
+        processed
+          .split("")
+          .reverse()
+          .join("") == processed
+      );
 }
 
 module.exports = palindromes
